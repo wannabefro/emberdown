334c8908e6274a45ba85bd3887e97e632f0fc744
Samuel McTaggart <sam.mctaggart@gmail.com>
2014-09-11 21:22:57 -0400

    can reopen file to reflect changes

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 var gui = require('nw.gui'),
-    fs = require('fs'),
-    marked = require('marked');
+fs = require('fs'),
+marked = require('marked');
 App = Ember.Application.create();
 
 App.Router.map(function() {
@@ -9,7 +9,8 @@ App.Router.map(function() {
 App.IndexRoute = Ember.Route.extend({
   model: function() {
     return {
-      body: ""
+      body: "",
+      path: null
     }
   }
 });
@@ -17,32 +18,33 @@ App.IndexRoute = Ember.Route.extend({
 App.ApplicationController = Ember.Controller.extend({
   needs: ['index'],
   index: Ember.computed.alias("controllers.index"),
+
   actions: {
     openFile: function() {
       var that = this;
       var chooser = $('#fileDialog');
       chooser.change(function(e) {
-        fs.readFile($(this).val(), function(err, data) {
-          if (err)
-            alert('Sorry something went wrong');
-          that.set('index.content.body', data.toString());
-          that.get('index').send('updateEditor');
-        });
+        var path = $(this).val();
+        if (path === '')
+          return;
+        that.get('index').send('openFile', path);
+        $(this).val('');
       });
       chooser.trigger('click');
     },
 
     saveFile: function() {
-      var that = this;
       var chooser = $('#fileSave');
-      chooser.trigger('click');
-      chooser.change(function(e) {
-        fs.writeFile($(this).val(), that.get('index.content.body'), function(err){
-          if (err)
-            alert('Something went wrong. Sorry :(');
-        })
-      })
+      var path;
+      if (!this.get('index.content.path')) {
+        chooser.trigger('click');
+        chooser.change(function(e) {
+          path = $(this).val();
+        });
+      };
+      this.get('index').send('writeFile', path);
     }
+
   }
 });
 
@@ -52,13 +54,16 @@ App.ApplicationView = Ember.View.extend({
     var win = gui.Window.get();
     var menubar = new gui.Menu({ type: 'menubar' });
     menubar.createMacBuiltin('Emberdown');
+
     var file = new gui.Menu();
+
     file.insert(new gui.MenuItem({
       label: 'Open',
       click: function() {
         that.get('controller').send('openFile');
       }
     }));
+
     file.insert(new gui.MenuItem({
       label: 'Save',
       click: function() {
@@ -74,8 +79,10 @@ App.IndexView = Ember.View.extend({
   editor: null,
   didInsertElement: function() {
     this.editor = ace.edit("editor");
+    this.controller.set('editor', this.editor);
     this.editor.getSession().setMode("ace/mode/markdown");
     $('.ace_text-input').focus();
+
     marked.setOptions({
       gfm: true,
       breaks: true,
@@ -84,12 +91,15 @@ App.IndexView = Ember.View.extend({
       }
     });
   },
+
   keyUp: function() {
     this.get('controller').set('body', this.editor.getValue());
   }
 });
 
 App.IndexController = Ember.ObjectController.extend({
+  editor: null,
+
   renderedMarkdown: function() {
     var markdown = marked(this.get('body'));
     return markdown;
@@ -98,6 +108,30 @@ App.IndexController = Ember.ObjectController.extend({
   actions: {
     updateEditor: function() {
       ace.edit("editor").setValue(this.get('body'));
+    },
+
+    openFile: function(path) {
+      var that = this;
+      fs.readFile(path, function(err, data) {
+        if (err)
+          alert('Sorry something went wrong :(');
+        that.set('body', data.toString());
+        that.set('path', path);
+        that.send('updateEditor');
+        that.editor.clearSelection();
+      });
+    },
+
+    writeFile: function(path) {
+      var that = this;
+      var path = path || this.get('path');
+      fs.writeFile(path, this.get('body'), function(err){
+        if (err) {
+          alert('Something went wrong. Sorry :(');
+        } else {
+          that.set('path', path);
+        }
+      });
     }
   }
 });
